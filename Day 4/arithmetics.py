a645dc6a60990e45679d381329697d5b3193005b
Akash Tiwari <154492266+shounak-tiwari@users.noreply.github.com>
2024-10-18 19:00:56 +0530

    Add files via upload

---
+++ Day 4/arithmetics.py
@@ -0,0 +1,9 @@
+a =10
+b =2
+print("the sum of a and b : ",a+b)
+print("the sub of a and b : ",a-b)
+print("the multiplication of a and b : ",a*b)
+print("the diviosion decimals of a and b : ",a/b)
+print("the remainder of a and b : ",a%b)
+print("the div in int of a and b : ",a//b)
+print("the expo power of a and b : ",a**b)
